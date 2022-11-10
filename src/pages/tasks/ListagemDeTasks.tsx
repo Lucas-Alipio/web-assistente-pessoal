f524dbce2bf4327e6a11dcf595047ff7561fc001
Lucas-Alipio <lucasantonio@gec.inatel.br>
2022-11-10 19:56:43 -0300

    ✨ feat: hook debounce

--- src/pages/tasks/ListagemDeTasks.tsx
+++ src/pages/tasks/ListagemDeTasks.tsx
@@ -4,12 +4,14 @@ import { useSearchParams } from "react-router-dom";
 import { UsersService } from "../../shared/services/api/users/UsersService";
 import { FerramentasDaListagem } from "../../shared/components";
 import { BaseLayoutPage } from "../../shared/layouts";
+import { useDebounce } from "../../shared/hooks";
 
 
 
 export const ListagemDeTasks: React.FC = () => {
 
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce();
 
   const search = useMemo(() => {
     return searchParams.get("search") || "";
@@ -17,15 +19,16 @@ export const ListagemDeTasks: React.FC = () => {
 
   useEffect(() => {
 
-    UsersService.getAll(1, search)
-    .then((result) => {
-      if ( result instanceof Error) {
-        alert(result.message);
-      } else {
-        console.log(result);
-      }
-    })
-
+    debounce(() => {
+      UsersService.getAll(1, search)
+        .then((result) => {
+          if ( result instanceof Error) {
+            alert(result.message);
+          } else {
+            console.log(result);
+          }
+        });
+    });
   }, [search]);
 
   return(
